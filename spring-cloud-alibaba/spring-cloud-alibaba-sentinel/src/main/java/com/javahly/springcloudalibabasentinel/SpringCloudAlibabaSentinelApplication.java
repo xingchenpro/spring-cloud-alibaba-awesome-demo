6139466b91b6f5871698ce22b086c55fc4375c11
黄良运 <1136513099@qq.com>
2020-03-15 19:26:55 +0800

    sentinel

--- spring-cloud-alibaba/spring-cloud-alibaba-sentinel/src/main/java/com/javahly/springcloudalibabasentinel/SpringCloudAlibabaSentinelApplication.java
+++ spring-cloud-alibaba/spring-cloud-alibaba-sentinel/src/main/java/com/javahly/springcloudalibabasentinel/SpringCloudAlibabaSentinelApplication.java
@@ -1,6 +1,5 @@
 package com.javahly.springcloudalibabasentinel;
 
-import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 

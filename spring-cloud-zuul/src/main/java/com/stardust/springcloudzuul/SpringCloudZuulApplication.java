67a03e868c5a513228c5591a02b03469e8e2d206
kiel <yaojinxin@banggood.com>
2021-05-26 18:47:13 +0800

    1

---
+++ spring-cloud-zuul/src/main/java/com/stardust/springcloudzuul/SpringCloudZuulApplication.java
@@ -0,0 +1,13 @@
+package com.stardust.springcloudzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudZuulApplication.class, args);
+    }
+
+}

fc07770a62286ca7efecb7cd1f06886d80ece12b
kiel <yaojinxin@banggood.com>
2021-05-26 17:34:13 +0800

    Initial commit

---
+++ spring-cloud-consul/src/test/java/com/stardust/springcloudconsul/SpringCloudConsulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stardust.springcloudconsul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootTest
+class SpringCloudConsulApplicationTests {
+
+
+
+    @Test
+    void contextLoads() {
+    }
+
+}

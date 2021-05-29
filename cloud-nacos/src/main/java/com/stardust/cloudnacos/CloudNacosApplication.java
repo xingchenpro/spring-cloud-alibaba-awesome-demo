c55918310b0abcca8f23199c20ee33113654fdd4
huangliangyun <1136513099@qq.com>
2021-05-29 20:54:35 +0800

    1

---
+++ cloud-nacos/src/main/java/com/stardust/cloudnacos/CloudNacosApplication.java
@@ -0,0 +1,13 @@
+package com.stardust.cloudnacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNacosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNacosApplication.class, args);
+	}
+
+}

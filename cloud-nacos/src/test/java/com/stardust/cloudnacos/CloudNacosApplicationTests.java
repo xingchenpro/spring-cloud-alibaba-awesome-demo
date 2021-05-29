392fc7e675ddef595fb816bde03ebfab9b1c22bc
huangliangyun <1136513099@qq.com>
2021-05-29 21:17:31 +0800

    1

---
+++ cloud-nacos/src/test/java/com/stardust/cloudnacos/CloudNacosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stardust.cloudnacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNacosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

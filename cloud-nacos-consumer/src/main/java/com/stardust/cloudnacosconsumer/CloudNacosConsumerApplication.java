13d5dcf3e9928aefe644d719377dc2a7021c7962
huangliangyun <1136513099@qq.com>
2021-05-31 22:11:57 +0800

    1

---
+++ cloud-nacos-consumer/src/main/java/com/stardust/cloudnacosconsumer/CloudNacosConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.stardust.cloudnacosconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNacosConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudNacosConsumerApplication.class, args);
+    }
+
+}

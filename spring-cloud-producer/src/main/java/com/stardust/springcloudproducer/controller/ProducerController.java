ad15c1e792383634a5faaed83c775e744680ee36
huangliangyun <1136513099@qq.com>
2021-06-09 01:35:08 +0800

    1

--- spring-cloud-producer/src/main/java/com/stardust/springcloudproducer/controller/ProducerController.java
+++ spring-cloud-producer/src/main/java/com/stardust/springcloudproducer/controller/ProducerController.java
@@ -1,7 +1,10 @@
 package com.stardust.springcloudproducer.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,9 +22,37 @@ public class ProducerController {
     @Value("${spring.cloud.client.ip-address}")
     private String ip;
 
+
     @GetMapping("/index")
     public String index() {
         return "ip:" + ip + ",port:" + port;
     }
+
+    @SentinelResource(value = "testLimit",blockHandler = "testLimitEx")
+    @RequestMapping("/testLimit")
+    public String testLimit() {
+        return "testLimit";
+    }
+
+    public String testLimitEx(BlockException e) {
+        return "接口已经被限流";
+    }
+
+
+    @SentinelResource(value = "testFallBack",fallback = "testFallBackEx")
+    @RequestMapping("/testFallBack")
+    public String testFallBack() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "testFallBack";
+    }
+
+    public String testFallBackEx() {
+        return "接口已经被熔断";
+    }
+
 
 }
